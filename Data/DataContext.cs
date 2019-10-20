a13492a83954358564658642af40b58a618cb521
josenchez69 <55004468+josenchez69@users.noreply.github.com>
2019-10-19 20:29:35 -0500

    prueba

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet <Registrar> UsuarioRegitrado { get; set;}
+    }
+}
