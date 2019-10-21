ae743214348168b064d03121ef83ff663fbd4659
josenchez69 <55004468+josenchez69@users.noreply.github.com>
2019-10-20 22:34:46 -0500

    Ajuste a test

--- Web.Tests/UnitTest1.cs
+++ Web.Tests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using Web.Controllers;
+using Web.Models;
 using Xunit;
 
 namespace Web.Tests
 {
     public class UnitTest1
     {
+        private readonly RegisterController _Contacto;
+
+        public UnitTest1()
+        {
+            _Contacto = new RegisterController();
+        }
+
         [Fact]
         public void Test1()
         {
-
+            var result = _Contacto.Login();
         }
     }
 }
